17d11343e1b208be761f2923de33e5cd79cd3995
zaidanr <72155553+mzaidanr088@users.noreply.github.com>
2020-10-15 17:17:24 +0700

    Add files via upload
    header baikinpesepeda

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>baikinpesepeda</title>
+</head>
+<body>
+    <header>
+        <!-- <h2>baikinpesepeda</h2> -->
+        <a href="index">baikinpesepeda</a>
+
+        <a href="rambu.php">Rambu</a>
+        <a href="rute.php">Rute</a>
+        <a href="game.php">Game</a>
+        <a href="akun.php">Akun</a>
+        <a href="login.php">Login</a>
+        <a href="register.php">Register</a>
+    </header>
+</body>
+</html>
