800c6a735d0c13bb6f263f6edbb71d592c1acb72
Umrohh <masfuumroh111@gmail.com>
2020-10-16 22:44:49 +0700

    registrasi

---
+++ registrasi.php
@@ -0,0 +1,47 @@
+<?php
+class registration{
+	
+
+	protected $inputName;
+	protected $inputusername;
+	protected $inputEmail;
+	protected $inputpassword;
+
+	private $registeredusername = 'masfuumr17';
+
+	public function __construct($inputName, $inputusername, $inputEmail, $inputpassword){
+		$this->inputName = $inputName;
+		$this->inputusername = $inputusername;
+		$this->inputEmail = $inputEmail;
+		$this->inputpassword = $inputpassword;
+	}
+	public function get_inputName(){
+		return $this->inputName;
+	}
+	public function get_inputusername(){
+		return $this->inputusername;
+	}
+	public function get_inputEmail(){
+		return $this->inputEmail;
+	}
+	public function get_inputpassword(){
+		return $this->inputpassword;
+	}
+	public function registeredusername(){
+		if($this->inputusername === $this->registeredusername ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	public function check(){
+		if($this->registeredusername()){
+			echo'username telah digunakan';
+		}else{
+			echo' Registrasi Success';
+		}
+	}
+}
+$registration = new registration ('umroh', 'umroh11', 'umroh@gmail.com', '1234');
+$registration->check();
+?>
